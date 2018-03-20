800cf5cc9603d857a98191d7c7d585d7af6c1172
Yizhi Feng <fengy2@rose-hulman.edu>
2018-03-20 12:05:08 -0400

    Initial Commit

---
+++ ScoreKeeperPrep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoreKeeperPrep
+//
+//  Created by FengYizhi on 2018/3/20.
+//  Copyright © 2018年 FengYizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by FengYizhi on 2018/3/20.
+//  Copyright © 2018年 FengYizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
